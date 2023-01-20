1f9cb9fb6a0d21886a2ff94fdaf48ab47fa29fd9
buytfdip <buytfdip@gmail.com>
2023-01-20 15:43:18 -0500

    debugged errors with spelling

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from core.config import settings
 from apis.version1.route_general_pages import general_pages_router
 from db.session import engine  
 from db.base import Base
+from db.session import engine   #new
+from db.base_class import Base  #new
 
 def include_router(app):
 	app.include_router(general_pages_router)
